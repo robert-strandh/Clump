48457933222b641c5c73eabdf4ef79f9843a5fd5
Robert Strandh <robert.strandh@gmail.com>
2015-11-19 07:51:17 +0100

    Finish implementation of ROTATE-LEFT.

--- Binary-tree/rotation.lisp
+++ Binary-tree/rotation.lisp
@@ -7,6 +7,32 @@
 (defmethod rotate-left (tree)
   (error 'invalid-binary-tree :tree tree))
 
+;;; Given a tree with the following shape:
+;;;
+;;;         tree
+;;;        /    \
+;;;       /      \
+;;;      l       r
+;;;             / \
+;;;            /   \
+;;;           rl   rr
+;;;
+;;; we turn it into a tree with this shape:
+;;;
+;;;           r
+;;;          / \
+;;;         /   \
+;;;       tree  rr
+;;;       /   \
+;;;      /     \
+;;;     l      rl
+;;;
+;;; The node R is returned
 (defmethod rotate-left ((tree node))
   (when (null (right tree))
     (error 'right-subtree-must-be-non-nil :tree tree))
+  (let* ((r (right tree))
+	 (rl (left r)))
+    (setf (right tree) rl
+	  (left r) tree)
+    r))

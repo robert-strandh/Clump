cb62499fab4836915e092f7e8f7d9e8043dedee4
Robert Strandh <robert.strandh@gmail.com>
2015-11-19 09:01:55 +0100

    Added English condition reporter for INVALID-BINARY-TREE.

--- Binary-tree/condition-reporters-english.lisp
+++ Binary-tree/condition-reporters-english.lisp
@@ -1,1 +1,11 @@
 (cl:in-package #:clump-binary-tree)
+
+(defmethod acclimation:report-condition
+    ((condition invalid-binary-tree)
+     stream
+     (language acclimation:english))
+  (format stream
+	  "A binary tree was expected,~@
+           but instead the following was found:~@
+           ~s"
+	  (tree condition)))

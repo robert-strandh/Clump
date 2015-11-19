fc571fc07ce26bf272c7b2cf098e3ee228f49389
Robert Strandh <robert.strandh@gmail.com>
2015-11-19 07:55:16 +0100

    Add English documentation for function (SETF LEFT).

--- Binary-tree/docstrings-english.lisp
+++ Binary-tree/docstrings-english.lisp
@@ -9,3 +9,10 @@
       (format nil "Given a binary tree, return the right child of that tree.~@
                    If an object other than a binary tree is given,~@
                    then an error of type INVALID-BINARY-TREE is signaled."))
+
+(setf (documentation '(setf left) 'function)
+      (format nil "Given two binary trees, NEW and TREE,~@
+                   make NEW the new left child of TREE.~@
+                   NEW may be a node or NIL.~@
+                   If TREE is NIL an error of type,~@
+                   INVALID-BINARY-TREE is signaled."))

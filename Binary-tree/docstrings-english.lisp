f326d3660b32c3e434d65da9f05a9e3dcc3110b6
Robert Strandh <robert.strandh@gmail.com>
2015-11-19 05:49:20 +0100

    Add docstring for function RIGHT.

--- Binary-tree/docstrings-english.lisp
+++ Binary-tree/docstrings-english.lisp
@@ -1,8 +1,11 @@
 (cl:in-package #:clump-binary-tree)
 
 (setf (documentation 'left 'function)
-      (format nil "Given a binary tree, obtain the left child of that tree.~@
+      (format nil "Given a binary tree, return the left child of that tree.~@
                    If an object other than a binary tree is given,~@
                    then an error of type INVALID-BINARY-TREE is signaled."))
 
-	      
+(setf (documentation 'right 'function)
+      (format nil "Given a binary tree, return the right child of that tree.~@
+                   If an object other than a binary tree is given,~@
+                   then an error of type INVALID-BINARY-TREE is signaled."))

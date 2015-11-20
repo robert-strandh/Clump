65ccec13a26835f4375d0a507e60853c21b37540
Robert Strandh <robert.strandh@gmail.com>
2015-11-20 10:24:43 +0100

    Define class NODE for splay trees.

--- Splay-tree/splay-tree.lisp
+++ Splay-tree/splay-tree.lisp
@@ -1,5 +1,8 @@
 (cl:in-package #:clump-splay-tree)
 
+(defclass node (clump-binary-tree:node)
+  ((%parent :initarg :parent :initform nil :accessor parent)))
+
 (defgeneric splay-step (node))
 
 (defgeneric splay (node))

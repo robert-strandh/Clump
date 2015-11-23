2a724effce9aa45f71e337b8d3c1159982a16380
Robert Strandh <robert.strandh@gmail.com>
2015-11-23 11:12:12 +0100

    Define default method on function SPLAY-STEP.

--- Splay-tree/splay-tree.lisp
+++ Splay-tree/splay-tree.lisp
@@ -1,7 +1,34 @@
 (cl:in-package #:clump-splay-tree)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Generic function SPLAY-STEP-WITH-PARENT.
+;;;
+;;; This function should not be called directly by client code.  It is
+;;; called by the function SPLAY-STEP with the NODE to splay and the
+;;; PARENT of NODE, only when PARENT is not NIL.
+;;;
+;;; Client code can define methods on this generic function.  Such
+;;; methods may be specialized to strict subclasses of the class
+;;; NODE-WITH-PARENT.
+;;;
+;;; This function returns NODE as a single value.
+
 (defgeneric splay-step-with-parent (node parent))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Generic function SPLAY-STEP.
+;;;
+;;; This function takes a NODE and applies a single splay step to that
+;;; node.
+
 (defgeneric splay-step (node))
+
+(defmethod splay-step ((node clump-binary-tree:node-with-parent))
+  (let ((parent (clump-binary-tree:parent node)))
+    (unless (null parent)
+      (splay-step-with-parent node parent)))
+  node)
 
 (defgeneric splay (node))

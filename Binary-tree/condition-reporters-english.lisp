8eb8c5997ae0ae121e61362f46064e0b421da7e4
Robert Strandh <robert.strandh@gmail.com>
2015-11-23 09:04:17 +0100

    Add English condition reporter for RIGHT-SUBTREE-MUST-BE-NON-NIL.

--- Binary-tree/condition-reporters-english.lisp
+++ Binary-tree/condition-reporters-english.lisp
@@ -9,3 +9,13 @@
            but instead the following was found:~@
            ~s"
 	  (tree condition)))
+
+(defmethod acclimation:report-condition
+    ((condition right-subtree-must-be-non-nil)
+     stream
+     (language acclimation:english))
+  (format stream
+	  "The right subtree of the following tree,~@
+           is NIL, but a non-NIL subtree is required:~@
+           ~s"
+	  (tree condition)))

fe6080418f101aa9fc88e7396f0370748c79ccff
Robert Strandh <robert.strandh@gmail.com>
2015-11-24 14:54:53 +0100

    Add comment explaining what ITERATIVE-TRAVERSAL does.

--- Binary-tree/iterative-traversal.lisp
+++ Binary-tree/iterative-traversal.lisp
@@ -1,5 +1,25 @@
 (cl:in-package #:clump-binary-tree)
 
+;;;; Depth-first iterative traversal.
+;;;;
+;;;; PRE, IN, and POST are functions of a single variable.  Each of
+;;;; these functions is called with a NODE as its argument.
+;;;;
+;;;; Before a node is visited, PRE is called with that node as an
+;;;; argument.  If PRE returns true, this is an indication that the
+;;;; left sub-tree of that node should be visited.  Otherwise, the
+;;;; current node is treated as if its left sub-tree were NIL.
+;;;;
+;;;; After the left sub-tree of some node has been visited, IN is
+;;;; called with that node as an argument.  If IN returns true, this
+;;;; is an indication that the right sub-tree of that node should be
+;;;; visited.  Otherwise, the current node is treated as if its right
+;;;; sub-tree were NIL.
+;;;;
+;;;; After the right sub-tree of some node has been visited, POST is
+;;;; called with that node as an argument.  The return value of POST
+;;;; is ignored.
+
 (defgeneric iterative-traversal (tree pre in post))
 
 (defmethod iterative-traversal ((tree node-with-parent) pre in post)

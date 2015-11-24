bc4ed48df13a0277d807a137e3465729ce79beb1
Robert Strandh <robert.strandh@gmail.com>
2015-11-24 14:45:34 +0100

    Complete rewrite using a state machine.

--- Binary-tree/iterative-traversal.lisp
+++ Binary-tree/iterative-traversal.lisp
@@ -1,25 +1,34 @@
 (cl:in-package #:clump-binary-tree)
 
-(defgeneric iterative-traversal (tree enter leave))
+(defgeneric iterative-traversal (tree pre in post))
 
-(defmethod iterative-traversal ((tree node-with-parent) enter leave)
+(defmethod iterative-traversal ((tree node-with-parent) pre in post)
   (let ((current-node tree))
-    (loop do (if (or (not (funcall enter current-node))
-		     (and (null (left current-node))
-			  (null (right current-node))))
-		 (loop for parent = (parent current-node)
-		       until (or (null parent)
-				 (and (eq current-node (left parent))
-				      (not (null (right parent)))))
-		       do (funcall leave current-node)
-			  (setf current-node parent)
-		       finally (funcall leave current-node)
-			       (if (null parent)
-				   (return-from iterative-traversal)
-				   (setf current-node (right parent))))
-		 (setf current-node
-		       (or (left current-node)
-			   (right current-node)))))))
-		 
-		 
-    
+    (tagbody
+     pre
+       (if (funcall pre current-node)
+	   (let ((left (left current-node)))
+	     (if (null left)
+		 (go in)
+		 (progn (setf current-node left)
+			(go pre))))
+	   (go in))
+     in
+       (if (funcall in current-node)
+	   (let ((right (right current-node)))
+	     (if (null right)
+		 (go post)
+		 (progn (setf current-node right)
+			(go pre))))
+	   (go post))
+     post
+       (funcall post current-node)
+       (let ((parent (parent current-node)))
+	 (if (null parent)
+	     (return-from iterative-traversal)
+	     (if (eq current-node (left parent))
+		 (progn (setf current-node parent)
+			(go in))
+		 (progn (setf current-node parent)
+			(go post))))))))
+

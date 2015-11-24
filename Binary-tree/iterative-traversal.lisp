a895353a5b1f1d4b26a52df37cabd640f1dd6f72
Robert Strandh <robert.strandh@gmail.com>
2015-11-24 09:12:51 +0100

    New component for traversing a binary tree iteratively.

---
+++ Binary-tree/iterative-traversal.lisp
@@ -0,0 +1,23 @@
+(cl:in-package #:clump-binary-tree)
+
+(defgeneric iterative-traversal (tree visit))
+
+(defmethod iterative-traversal ((tree node-with-parent) visit)
+  (let ((current-node tree))
+    (loop do (if (or (not (funcall visit current-node))
+		     (and (null (left current-node))
+			  (null (right current-node))))
+		 (loop for parent = (parent current-node)
+		       until (or (null parent)
+				 (and (eq current-node (left parent))
+				      (not (null (right parent)))))
+		       do (setf current-node parent)
+		       finally (if (null parent)
+				   (return-from iterative-traversal)
+				   (setf current-node (right parent))))
+		 (setf current-node
+		       (or (left current-node)
+			   (right current-node)))))))
+		 
+		 
+    

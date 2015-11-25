9a1a36497b64cf0d0c1a42ecef08bce17c5321e6
Robert Strandh <robert.strandh@gmail.com>
2015-11-25 10:57:39 +0100

    Add required parameter for the class of the nodes to create.

--- Test/random-tree.lisp
+++ Test/random-tree.lisp
@@ -1,9 +1,9 @@
 (cl:in-package #:clump-test)
 
-(defun random-tree (&optional (node-probability 1.0))
+(defun random-tree (node-class &optional (node-probability 1.0))
   (if (< (random 1.0) node-probability)
-      (make-instance 'node
+      (make-instance node-class
 	:data (random 20)
-	:left (random-tree (* 0.9 node-probability))
-	:right (random-tree (* 0.9 node-probability)))
+	:left (random-tree node-class (* 0.9 node-probability))
+	:right (random-tree node-class (* 0.9 node-probability)))
       nil))

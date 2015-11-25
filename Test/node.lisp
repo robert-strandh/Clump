d17dceca7b35f33d99940413078a55603e1a3f2b
Robert Strandh <robert.strandh@gmail.com>
2015-11-25 10:59:01 +0100

    Add new class for nodes with no parent slot.

--- Test/node.lisp
+++ Test/node.lisp
@@ -1,4 +1,7 @@
 (cl:in-package #:clump-test)
 
+(defclass simple-node (clump-binary-tree:simple-node)
+  ((%data :initarg :data :reader data)))
+
 (defclass node (clump-binary-tree:node-with-parent)
   ((%data :initarg :data :reader data)))

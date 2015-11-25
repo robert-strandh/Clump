f0d4e95f26f3a88f68425d651f7cd0daab98093c
Robert Strandh <robert.strandh@gmail.com>
2015-11-25 11:12:40 +0100

    Add test for trees with simple nodes.

--- Test/test.lisp
+++ Test/test.lisp
@@ -7,3 +7,10 @@
 	for tree = (random-tree 'node-with-parent)
 	do (fixup-parents tree)
 	   (assert (tree-equal (traverse tree r) (traverse tree i)))))
+
+(defun test2 ()
+  (loop with r = #'clump-binary-tree:recursive-traversal
+	with i = #'clump-binary-tree:iterative-traversal
+	repeat 10000
+	for tree = (random-tree 'simple-node)
+	do (assert (tree-equal (traverse tree r) (traverse tree i)))))

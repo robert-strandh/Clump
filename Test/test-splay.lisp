5a0c4f30dd135e9e2da21826acb459e15692f1f4
Robert Strandh <robert.strandh@gmail.com>
2015-11-27 08:29:30 +0100

    Add comment explaining how we test the SPLAY operation.

--- Test/test-splay.lisp
+++ Test/test-splay.lisp
@@ -1,5 +1,16 @@
 (cl:in-package #:clump-test)
 
+;;; We know that the SPLAY operation preserves an in-order traversal
+;;; of the tree.  Therefore, we test the SPLAY operation by the
+;;; following steps:
+;;;
+;;;  1. Generate a random tree
+;;;  2. Make an in-order traversal of that tree, saving the result
+;;;  3. Select a random node of the random tree.
+;;;  4. Splay the selected node.
+;;;  5. Make an in-order traversal of the splayed node, saving the result.
+;;;  6. Compare the two in-order traversals.
+
 (defun in-order-traversal (tree)
   (let ((result '()))
     (clump-binary-tree:recursive-traversal

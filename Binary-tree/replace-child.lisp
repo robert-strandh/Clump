e5c5fca9002a1028beb3bda9994df49a88e15d3d
Robert Strandh <robert.strandh@gmail.com>
2015-11-28 07:11:05 +0100

    Add method on REPLACE-CHILD specialized to NODE.

--- Binary-tree/replace-child.lisp
+++ Binary-tree/replace-child.lisp
@@ -4,3 +4,13 @@
 
 (defmethod replace-child ((node null) (old-child node) new-child)
   new-child)
+
+(defmethod replace-child ((node node) (old-child node) new-child)
+  (cond ((eq old-child (left node))
+	 (setf (left node) new-child))
+	((eq old-child (right node))
+	 (setf (right node) new-child))
+	(t
+	 (error 'must-be-child
+		:tree node
+		:child old-child))))

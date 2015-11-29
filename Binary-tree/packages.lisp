4135d2a3bdf80f758177fffeae9e6421ab85c32b
Robert Strandh <robert.strandh@gmail.com>
2015-11-29 08:28:03 +0100

    Export names of functions for splaying.

--- Binary-tree/packages.lisp
+++ Binary-tree/packages.lisp
@@ -15,6 +15,15 @@
 	   #:replace-child
 	   #:recursive-traversal
 	   #:iterative-traversal
+	   #:splay-step-with-left-parent-and-left-grandparent
+	   #:splay-step-with-left-parent-and-right-grandparent
+	   #:splay-step-with-right-parent-and-left-grandparent
+	   #:splay-step-with-right-parent-and-right-grandparent
+	   #:splay-step-with-left-parent
+	   #:splay-step-with-right-parent
+	   #:splay-step-with-parent
+	   #:splay-step
+	   #:splay
 	   #:invalid-binary-tree
 	   #:right-subtree-must-be-non-nil
 	   #:left-subtree-must-be-non-nil))

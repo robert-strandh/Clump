cb8b58f2a841f6b02f4ef656939a890077d219b3
Robert Strandh <robert.strandh@gmail.com>
2016-01-04 10:18:50 +0100

    Rename parent parameter for improved readability.

--- Binary-tree/link-unlink.lisp
+++ Binary-tree/link-unlink.lisp
@@ -1,13 +1,14 @@
 (cl:in-package #:clump-binary-tree)
 
 ;;; Make CHILD the left child of NODE.
-(defgeneric link-left (node child))
+(defgeneric link-left (parent child))
 
-;;; Make CHILD the right child of NODE.
-(defgeneric link-right (node child))
+;;; Make CHILD the right child of PARENT.
+(defgeneric link-right (parent child))
 
-;;; Remove CHILD as the left child of NODE.
-(defgeneric unlink-left (node child))
+;;; Remove CHILD as the left child of PARENT.
+(defgeneric unlink-left (parent child))
 
-;;; Remove CHILD as the right child of NODE.
-(defgeneric unlink-right (node child))
+;;; Remove CHILD as the right child of PARENT.
+(defgeneric unlink-right (parent child))
+

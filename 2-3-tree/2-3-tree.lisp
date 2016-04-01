a8115fcdfddf6b9c68715498cc06ef1ef840a1a2
Robert Strandh <robert.strandh@gmail.com>
2016-04-01 15:57:22 +0200

    Add slots in TREE class containing node classes.

--- 2-3-tree/2-3-tree.lisp
+++ 2-3-tree/2-3-tree.lisp
@@ -1,7 +1,10 @@
 (cl:in-package #:clump-2-3-tree)
 
 (defclass tree ()
-  ((%contents :initform nil :accessor contents)))
+  ((%contents :initform nil :accessor contents)
+   (%leaf-class :initarg :leaf-class :reader leaf-class)
+   (%2-node-class :initarg :2-node-class :reader 2-node-class)
+   (%3-node-class :initarg :3-node-class :reader 3-node-class)))
 
 (defclass node ()
   ((%parent :initarg :parent :accessor parent)))

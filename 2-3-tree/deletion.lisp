0eed17757ff0a75a5a218cf589d2b0af8984548e
Robert Strandh <robert.strandh@gmail.com>
2016-04-03 12:45:00 +0200

    Define method on DELETE-CHILD specialized to 3-NODE.

--- 2-3-tree/deletion.lisp
+++ 2-3-tree/deletion.lisp
@@ -18,3 +18,20 @@
 		      (if (eq child (left node))
 			  (right node)
 			  (left node))))
+
+(defmethod delete-child ((node 3-node) child)
+  (let ((2-node-class (2-node-class (tree node))))
+    (if (eq child (left node))
+	(replace-and-shrink (parent node)
+			    (make-instance 2-node-class
+			      :left (middle node)
+			      :right (right node)))
+	(if (eq child (middle node))
+	    (replace-and-shrink (parent node)
+				(make-instance 2-node-class
+				  :left (left node)
+				  :right (right node)))
+	    (replace-and-shrink (parent node)
+				(make-instance 2-node-class
+				  :left (left node)
+				  :right (middle node)))))))

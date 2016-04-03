bff0b32fb25ad5ae28537525c4d8ff756f467cea
Robert Strandh <robert.strandh@gmail.com>
2016-04-03 07:47:57 +0200

    New condition type for insertions in non-empty tree.

--- 2-3-tree/conditions.lisp
+++ 2-3-tree/conditions.lisp
@@ -2,3 +2,6 @@
 
 (define-condition 2-3-tree-error (error acclimation:condition)
   ())
+
+(define-condition tree-for-insertion-must-be-empty (2-3-tree-error)
+  ((%tree :initarg :tree :reader tree)))

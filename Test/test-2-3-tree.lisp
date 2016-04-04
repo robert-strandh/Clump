df9f168fa4bfa6af372a2c788de9e3ff6db4fc60
Robert Strandh <robert.strandh@gmail.com>
2016-04-04 11:40:23 +0200

    Define subclasses of 2-3 tree classes for testing purposes.

--- Test/test-2-3-tree.lisp
+++ Test/test-2-3-tree.lisp
@@ -1,2 +1,20 @@
 (cl:in-package #:clump-test)
 
+(defclass size-mixin ()
+  ((%size :initarg :size :reader size)))
+
+(defclass leaf-size (size-mixin clump-2-3-tree:leaf)
+  ()
+  (:default-initargs :size 1))
+
+(defclass 2-node-size (size-mixin clump-2-3-tree:2-node)
+  ())
+
+(defclass 3-node-size (size-mixin clump-2-3-tree:3-node)
+  ())
+
+(defclass size-tree (clump-2-3-tree:tree)
+  ()
+  (:default-initargs
+   :2-node-class '2-node-size
+   :3-node-class '3-node-size))

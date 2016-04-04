447005d260f8ba1e5a5993cb40304fb7951deb7c
Robert Strandh <robert.strandh@gmail.com>
2016-04-04 11:53:47 +0200

    Factor out size computation to a different generic function.

--- Test/test-2-3-tree.lisp
+++ Test/test-2-3-tree.lisp
@@ -10,11 +10,16 @@
 (defclass 2-node-size (size-mixin clump-2-3-tree:2-node)
   ())
 
-(defmethod initialize-instance :after ((object 2-node-size) &key)
+(defgeneric recompute-size (node))
+
+(defmethod recompute-size ((node 2-node-size))
   (reinitialize-instance
-   object
-   :size (+ (size (clump-2-3-tree:left object))
-	    (size (clump-2-3-tree:right object)))))
+   node
+   :size (+ (size (clump-2-3-tree:left node))
+	    (size (clump-2-3-tree:right node)))))
+
+(defmethod initialize-instance :after ((object 2-node-size) &key)
+  (recompute-size object))
 
 (defclass 3-node-size (size-mixin clump-2-3-tree:3-node)
   ())

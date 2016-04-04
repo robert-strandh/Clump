cb0b6ee8fa3a3e6b665a39cf7be42e04d890a7a8
Robert Strandh <robert.strandh@gmail.com>
2016-04-04 11:44:11 +0200

    Define :AFTER method on 2-node-size that computes sum of child sizes.

--- Test/test-2-3-tree.lisp
+++ Test/test-2-3-tree.lisp
@@ -10,6 +10,12 @@
 (defclass 2-node-size (size-mixin clump-2-3-tree:2-node)
   ())
 
+(defmethod initialize-instance :after ((object 2-node-size) &key)
+  (reinitialize-instance
+   object
+   :size (+ (size (clump-2-3-tree:left object))
+	    (size (clump-2-3-tree:right object)))))
+
 (defclass 3-node-size (size-mixin clump-2-3-tree:3-node)
   ())
 
